ad8279c4490f7768c0ded544cc6f89a71e7c183b
diego <diegoesteban1881@gmail.com>
2023-03-21 15:56:34 -0500

    base application

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd829cb9b-e621-4f37-b570-3ebea791adc7';
+export const MODAL_CLOSE_SEC = 2.5;
